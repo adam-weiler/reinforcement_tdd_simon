335d8bf4925b7f45ef53484fbda635d7a5bf47b5
adam-weiler <adam.weiler@yahoo.ca>
2019-09-04 12:36:02 -0400

    Testing hello() is successful.

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,23 @@
+const echo = (word) => {
+    return word;
+}
+
+const shout = () => {
+    
+}
+
+const repeat = () => {
+    
+}
+
+const startOfWord = () => {
+    
+}
+
+const firstWord = () => {
+    
+}
+
+
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
--- simon_says.test.js
+++ simon_says.test.js
@@ -12,28 +12,28 @@ test('echo bye', () => {
   expect(echo('bye')).toEqual('bye');
 });
 
-test('shout hello', () => {
-  expect(shout('hello')).toEqual('HELLO');
-});
+// test('shout hello', () => {
+//   expect(shout('hello')).toEqual('HELLO');
+// });
 
-test('shout multiple words', () => {
-  expect(shout('hello world')).toEqual('HELLO WORLD');
-});
+// test('shout multiple words', () => {
+//   expect(shout('hello world')).toEqual('HELLO WORLD');
+// });
 
-test('repeat', () => {
-  expect(repeat('hello', 3)).toEqual('hello hello hello');
-});
+// test('repeat', () => {
+//   expect(repeat('hello', 3)).toEqual('hello hello hello');
+// });
 
-test('start of word returns first letter', () => {
-  expect(startOfWord('hello', 1)).toEqual('h');
-});
+// test('start of word returns first letter', () => {
+//   expect(startOfWord('hello', 1)).toEqual('h');
+// });
 
 
-test('start of word returns first two letters', () => {
-  expect(startOfWord('Bob', 2)).toEqual('Bo');
-});
+// test('start of word returns first two letters', () => {
+//   expect(startOfWord('Bob', 2)).toEqual('Bo');
+// });
 
-test('first word', () => {
-  expect(firstWord('Hello World')).toEqual('Hello');
-});
+// test('first word', () => {
+//   expect(firstWord('Hello World')).toEqual('Hello');
+// });
 
